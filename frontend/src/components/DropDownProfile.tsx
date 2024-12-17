33fd6562eb6baf43804e963f9f1c69a0fc446029
Juan Alberto <jarscode.dev@outlook.com>
2024-12-17 19:46:49 +0100

    fix: dropdown menu

--- frontend/src/components/DropDownProfile.tsx
+++ frontend/src/components/DropDownProfile.tsx
@@ -41,7 +41,7 @@ export const DropDownProfile = () => {
   if (isLoading) return <Loading />;
 
   const menuItemStyle =
-    "flex gap-1.5 items-center px-4 py-2 text-sm font-semibold rounded text-neutral-900 dark:text-neutral-100 hover:bg-neutral-100 dark:hover:bg-slate-800";
+    "flex gap-1.5 items-center cursor-default px-1 py-2 text-sm font-semibold rounded text-neutral-900 dark:text-neutral-100 hover:bg-neutral-100 dark:hover:bg-slate-800";
 
   return (
     <>
@@ -64,13 +64,13 @@ export const DropDownProfile = () => {
           {/* Dropdown Menu */}
           {isDropOpen && (
             <div
-              className="absolute right-0 z-20 px-2 py-2 mt-2 transition duration-300 ease-in-out origin-top transform scale-y-100 bg-white rounded-md shadow-lg opacity-100 border-neutral-800 top-5 w-60 dark:bg-neutral-950 dark:border-zinc-300"
+              className="absolute right-0 z-20 px-2 mt-2 transition duration-300 ease-in-out origin-top transform scale-y-100 bg-white rounded-md shadow-lg opacity-100 border-neutral-800 top-5 w-60 dark:bg-neutral-950 dark:border-zinc-300"
             >
-              <div className="px-4">
+              <div className="pt-4 mb-2 border-b border-neutral-200 dark:border-neutral-800">
                 <p className="text-[15px] text-neutral-900 font-semibold dark:text-neutral-100">
                   {user?.full_name}
                 </p>
-                <p className="mb-4 text-sm opacity-75 text-neutral-900 dark:text-neutral-100">
+                <p className="mb-2 text-sm opacity-75 text-neutral-900 dark:text-neutral-100">
                   {user?.email}
                 </p>
               </div>
