566a91af493547e5059695d0f9435bc99be0b1fe
Juan Alberto <jarscode.dev@outlook.com>
2024-12-22 19:34:15 +0100

    update: vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
-      '^/(?!(dashboard|about|dashboard/settings|settings|signin|signup)$)[a-zA-Z0-9_-]+$': {
+      '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
         target: process.env.VITE_BACKEND_URL ? `${process.env.VITE_BACKEND_URL}/urls` : 'http://localhost:5000/urls',
         changeOrigin: true,
         rewrite: (path) => path.replace('/', ''),
