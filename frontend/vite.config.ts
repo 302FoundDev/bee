8f95930e2ccc76135107a6441f236d3c6a7e1ce3
Juan Alberto <jarscode.dev@outlook.com>
2024-12-28 22:42:14 +0100

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { BACKEND_URL } from './src/constants';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_URL ? `${process.env.VITE_BACKEND_URL}/urls` : 'http://localhost:5000/urls',
+        target: BACKEND_URL ? `${BACKEND_URL}/urls` : 'https://bee-api-gps3.onrender.com/urls',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
-        target: process.env.VITE_BACKEND_URL ? `${process.env.VITE_BACKEND_URL}/urls` : 'http://localhost:5000/urls',
+        target: BACKEND_URL ? `${BACKEND_URL}/urls` : 'https://bee-api-gps3.onrender.com/urls',
         changeOrigin: true,
         rewrite: (path) => path.replace('/', ''),
       },
