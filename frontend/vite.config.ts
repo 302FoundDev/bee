24682d4f39bdcca30b696923e19ee5ec2fe54bae
Juan Alberto <jarscode.dev@outlook.com>
2024-12-28 22:45:26 +0100

    fix: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,18 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { BACKEND_URL } from './src/constants';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: BACKEND_URL ? `${BACKEND_URL}/urls` : 'https://bee-api-gps3.onrender.com/urls',
+        target: `${import.meta.env.VITE_BACKEND_URL}/urls`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
-        target: BACKEND_URL ? `${BACKEND_URL}/urls` : 'https://bee-api-gps3.onrender.com/urls',
+        target: `${import.meta.env.VITE_BACKEND_URL}/urls`,
         changeOrigin: true,
         rewrite: (path) => path.replace('/', ''),
       },
