1b43ec2d14927e4ead0a80e2a40f5dbb23b33630
Juan Alberto <jarscode.dev@outlook.com>
2024-12-29 14:59:57 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
-
->>>>>>> e71fc77833942b1562de21ac8fbb2cb547d253d3
   // server: {
   //   proxy: {
   //     '/api': {
@@ -15,11 +11,8 @@ export default defineConfig({
   //       rewrite: (path) => path.replace(/^\/api/, ''),
   //     },
   //     '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
-<<<<<<< HEAD
   //       target: 'https://bee-api-gps3.onrender.com/urls',
-=======
   //       target: process.env.VITE_BACKEND_URL ? `https://bee-api-gps3.onrender.com/urls` : 'https://bee-api-gps3.onrender.com/urls',
->>>>>>> e71fc77833942b1562de21ac8fbb2cb547d253d3
   //       changeOrigin: true,
   //       rewrite: (path) => path.replace('/', ''),
   //     },
