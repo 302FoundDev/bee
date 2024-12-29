d0b79909a5ff4b1d659f99182e16504979d27a07
Juan Alberto <jarscode.dev@outlook.com>
2024-12-29 16:48:13 +0100

    Commit changes

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -11,4 +11,8 @@ declare global {
 
 // vite.config.ts
 export const BACKEND_URL = 'https://bee-api-gps3.onrender.com';
-export const FRONTEND_URL = 'https://beeslug.vercel.app';
+// export const FRONTEND_URL = 'https://beeslug.vercel.app';
+
+// Local development
+// export const BACKEND_URL = 'http://localhost:5000';
+export const FRONTEND_URL = 'http://localhost:5173';
