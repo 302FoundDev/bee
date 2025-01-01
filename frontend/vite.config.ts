5136c9d4960dac685123ada90b0c0fa36fb69f5a
Juan Alberto <jarscode.dev@outlook.com>
2025-01-02 00:30:17 +0100

    Remove proxy configuration from Vite server settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$|dashboard$)[a-zA-Z0-9_-]+$': {
-        target: 'https://bee-api-gps3.onrender.com/urls',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/+/, ''),
-      },
-    },
-  },
 });
