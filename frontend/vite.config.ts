3fa2c5b9f53681f2ee502b381e7680c441006565
Juan Alberto <jarscode.dev@outlook.com>
2025-01-01 23:27:15 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL ? `bee-api-backend.vercel.app/urls` : 'bee-api-backend.vercel.app/urls',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
       '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
         target: process.env.VITE_BACKEND_URL ? `bee-api-backend.vercel.app/urls` : 'bee-api-backend.vercel.app/urls',
         changeOrigin: true,
