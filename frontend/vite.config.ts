f73869fb4d5fc1555a82d5effd727863c5b063d5
Juan Alberto <jarscode.dev@outlook.com>
2025-01-01 23:26:38 +0100

    Enable API proxy configuration in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: process.env.VITE_BACKEND_URL ? `bee-api-backend.vercel.app/urls` : 'bee-api-backend.vercel.app/urls',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //     '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
-  //       target: process.env.VITE_BACKEND_URL ? `bee-api-backend.vercel.app/urls` : 'bee-api-backend.vercel.app/urls',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace('/', ''),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKEND_URL ? `bee-api-backend.vercel.app/urls` : 'bee-api-backend.vercel.app/urls',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '^/(?!dashboard(/settings)?$|about$|settings$|signin$|signup$)[a-zA-Z0-9_-]+$': {
+        target: process.env.VITE_BACKEND_URL ? `bee-api-backend.vercel.app/urls` : 'bee-api-backend.vercel.app/urls',
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/', ''),
+      },
+    },
+  },
 });
