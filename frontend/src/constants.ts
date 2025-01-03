ecd5668c990692da6aad1e03bae725401d3fef8e
Juan Alberto <jarscode.dev@outlook.com>
2025-01-03 05:44:05 +0100

    Refactor middleware: remove old middleware file, update routing in vercel.json, and adjust FRONTEND_URL export

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -11,8 +11,8 @@ declare global {
 
 // vite.config.ts
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
+// export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL;
 
 // Local development
 // export const BACKEND_URL = 'http://localhost:5000';
-// export const FRONTEND_URL = 'http://localhost:5173';
+export const FRONTEND_URL = 'http://localhost:5173';
