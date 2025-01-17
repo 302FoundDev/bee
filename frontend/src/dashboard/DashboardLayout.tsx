03769f198364d2d6a9ecc2e04433df90b84a0320
Juan Alberto <jarscode.dev@outlook.com>
2025-01-17 19:13:43 +0100

    Update subproject reference in bee-backend and add comment in DashboardLayout for clarity

--- frontend/src/dashboard/DashboardLayout.tsx
+++ frontend/src/dashboard/DashboardLayout.tsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom'
 export const DashboardLayout = () => {
 
   const location = useLocation()
+  // Layout dashboard with sidenav
 
   return (
     <section className="flex flex-col min-h-screen text-neutral-950 dark:text-neutral-100">
